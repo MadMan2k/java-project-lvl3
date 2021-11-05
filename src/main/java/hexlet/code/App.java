c98df984c1a62734b30e4f0e708fb538c5124cf8
igor.onuferko <igor.nfrk@gmail.com>
2021-11-05 20:42:35 +0100

    checkstyle fix

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,12 +1,5 @@
 package hexlet.code;
 
-import hexlet.code.schemas.BaseSchema;
-import hexlet.code.schemas.MapSchema;
-import hexlet.code.schemas.StringSchema;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class App {
     public static void main(String[] args) {
 //        Validator v = new Validator();
